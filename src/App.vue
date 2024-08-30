c939a980a5909bc3010d0febafdac04e46eaafd2
ManoelFranciscoDias <manoel2007dias@gmail.com>
2024-08-30 11:47:31 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted, reactive } from 'vue';
+
 
 const isMenuActive = ref(false);
 const currentSlide = ref(1);
@@ -8,21 +9,24 @@ function toggleMenu() {
   isMenuActive.value = !isMenuActive.value;
 }
 
-function nextSlide() {
-  currentSlide.value = currentSlide.value === 4 ? 1 : currentSlide.value + 1;
+const dados = reactive({
+  nome: '',
+  email: '',
+  cpf: '',
+
+})
+
+function validacaosenha() {
+  if (dados.nome == '' || dados.email == '' || dados.cpf == '') {
+
+    alert("Existem campos vazios")
+
+  }
+
 }
 
-let slideInterval;
 
-onMounted(() => {
-  slideInterval = setInterval(() => {
-    nextSlide();
-  }, 5000);
-});
 
-onUnmounted(() => {
-  clearInterval(slideInterval);
-});
 </script>
 
 <template>
@@ -35,10 +39,10 @@ onUnmounted(() => {
 
       <ul :class="['nav-menu', { active: isMenuActive }]">
         <li class="nav-item">
-          <router-link to="/categorias" class="nav-link">CATEGORIAS</router-link>
+          <router-link to="/categorias" class="nav-link">PEÇAS</router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/eventos" class="nav-link">EVENTOS</router-link>
+          <router-link to="/eventos" class="nav-link">CLIENTES</router-link>
         </li>
         <li class="nav-item">
           <router-link to="/sobre" class="nav-link">SOBRE</router-link>
@@ -54,61 +58,31 @@ onUnmounted(() => {
         <span class="bar"></span>
       </div>
 
-      <button class="btn">LOGIN</button>
+
     </nav>
   </header>
 
 
-  <div class="slider">
-    <div class="slides">
-      <!--radios-->
-      <input type="radio" name="radio-btn" id="radio1" v-model="currentSlide" value="1">
-      <input type="radio" name="radio-btn" id="radio2" v-model="currentSlide" value="2">
-      <input type="radio" name="radio-btn" id="radio3" v-model="currentSlide" value="3">
-      <input type="radio" name="radio-btn" id="radio4" v-model="currentSlide" value="4">
-
-      <!--imagens-->
-      <div class="slide first">
-        <img src="./assets/img/img-slide-1.jpg" alt="imagem1">
+  <div class="container">
+    <div style="color: white;" class="formulario">
+      <h1>CLIENTES</h1>
+      <div class="column">
+        <label for="">Nome:</label>
+        <input type="text" v-model="dados.nome">
+      </div>
+      <div class="column">
+        <label for="">Email:</label>
+        <input type="email" v-model="dados.email">
+      </div>
+      <div class="column">
+        <label for="">CPF:</label>
+        <input type="text" v-model="dados.cpf">
       </div>
 
-
-      <div class="slide">
-        <img src="./assets/img/img-slide-2.jpg" alt="imagem2">
-      </div>
-
-
-      <div class="slide">
-        <img src="./assets/img/img-slide-3.jpg" alt="imagem3">
-      </div>
-
-
-      <div class="slide">
-        <img src="./assets/img/img-slide-4.jpg" alt="imagem4">
-      </div>
-
-      <div class="navigation-auto">
-
-        <div class="auto-btn1"></div>
-        <div class="auto-btn2"></div>
-        <div class="auto-btn3"></div>
-        <div class="auto-btn4"></div>
-
-      </div>
-
-
-        <div class="manual-navigation">
-          <label for="radio1" class="manual-btn"></label>
-          <label for="radio2" class="manual-btn"></label>
-          <label for="radio3" class="manual-btn"></label>
-          <label for="radio4" class="manual-btn"></label>
-        </div>
-
-
-
-      </div>
+      <button @click="validacaosenha" class="btn">CADASTRAR</button>
     </div>
-  
+  </div>
+
 
 </template>
 
@@ -116,6 +90,7 @@ onUnmounted(() => {
 header {
   display: flex;
   flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 
 }
 
@@ -124,7 +99,7 @@ header {
 }
 
 .navbar {
-  background-color: rgb(0, 0, 0);
+  background-color: #0D0D0D;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -201,129 +176,59 @@ header {
 
 }
 
+.formulario {
+  background-color: #111111;
+  border-radius: 10px;
+  padding: 40px;
+  width: 40%;
+  display: flex;
+  margin-top: 50px;
+  flex-direction: column;
+  align-items: center;
+}
+
+.container {
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  margin-top: 2rem;
+}
+
+.column {
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 .btn {
-  background-color: rgb(65, 65, 65);
-  padding: 7px;
+  background-color: #2a5239;
   border: none;
-  border-radius: 7px;
-  transition: 0.3s;
+  height: 40px;
+  width: 30vh;
   color: white;
-  width: 100px;
+  border-radius: 10px;
+  margin-top: 20px;
 }
 
 .btn:hover {
-  background-color: rgb(131, 131, 131);
+  background-color: #2d6d45;
 }
 
-.slider {
-  margin: 0 auto;
-  width: 100%;
-  height: 400px;
-  overflow: hidden;
-  position: relative;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-  
-}
-
-.slides {
-  width: 400%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-
-}
-
-.slides input {
-  display: none;
-}
-
-.slide {
-  width: 25%;
-  position: relative;
-  transition: 1s;
-  height: 100%;
-  object-fit: cover;
-}
-
-.slide img {
-  width: 800px;
-
-}
-
-.manual-navigation {
-  position: absolute;
-  width: 100%;
-  margin-top: 360px;
-  display: flex;
-  justify-content: center;
-}
-
-.manual-btn {
-  border: 5px solid #ffffff;
-  padding: 5px;
+input {
+  background-color: rgb(41, 41, 41);
+  color: white;
+  width: 400px;
+  padding: 10px;
+  font-size: 15px;
   border-radius: 10px;
-  cursor: pointer;
-  transition: 0.5s;
+  margin-bottom: 10px;
+  border: none;
 }
 
-.manual-btn:not(:last-child) {
-  margin-right: 40px;
+input:hover {
+  background-color: rgb(27, 24, 24);
+  color: white;
+  border: none;
 }
-
-.manual-btn:hover {
-  background-color: #fff;
-}
-
-#radio1:checked ~ .first {
-  margin-left: 0;
-}
-
-#radio2:checked ~ .first {
-  margin-left: -25%;
-}
-
-#radio3:checked ~ .first {
-  margin-left: -50%;
-}
-
-#radio4:checked ~ .first {
-  margin-left: -75%;
-}
-
-.navigation-auto div {
-  border: 5px solid #20a6ff;
-  padding: 5px;
-  border-radius: 10px;
-  cursor: pointer;
-  transition: 1s;
-}
-
-.navigation-auto {
-  position: absolute;
-  width: 100%;
-  margin-top: 360px;
-  display: flex;
-  justify-content: center;
-}
-
-.navigation-auto div:not(:last-child) {
-  margin-right: 40px;
-}
-
-#radio1:checked ~ .navigation-auto .auto-btn1 {
-  background-color: #fff;
-}
-
-#radio2:checked ~ .navigation-auto .auto-btn2 {
-  background-color: #fff;
-}
-
-#radio3:checked ~ .navigation-auto .auto-btn3 {
-  background-color: #fff;
-}
-
-#radio4:checked ~ .navigation-auto .auto-btn4 {
-  background-color: #fff;
-}
-
 </style>
